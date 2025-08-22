0898ed9befebf61467de77c865bb9db03712502e
Giovani Ocan <giovaniocan@gmail.com>
2025-08-22 09:19:22 -0300

    first commit

---
+++ alface_roxa.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env  python3
+# -*- coding: utf-8 -*-
+
+"""
+Nome do Arquivo: alface_roxa.py
+Autor: Giovani Ocan
+Data de Criação: 21 de agosto de 2025
+Versão: 1.0
+
+Descrição:
+Esse arquivo é um teste para aprender sobre cabeçalhos padrões de script em python
+"""
